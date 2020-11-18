e8d6e0348d69c2900e93adf441a0947502dc5aea
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-18 22:30:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,10 +11,11 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  
+  
  # {[:railroads]} = {:pieces => 4, :rent_in_dollars => {}, :names => {}}
- {
-    :railroads => {}
-  }
+
+  
 end
 
 def monopoly_with_third_tier
