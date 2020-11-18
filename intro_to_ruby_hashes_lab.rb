314c28141fede79aad4f8775548bfe9d34ae068f
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-18 22:36:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,13 +12,12 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   my_hash = {
-    :railroads => {}
+    :railroads => {
+      :pieces => 4
+    }
   }
   
-  [my_hash][:railroads][:pieces] = 4
-  [my_hash][:railroads][:rent_in_dollars] = {}
-  [my_hash][:railroads][:names] = {}
-  #{:pieces => 4, :rent_in_dollars => {}, :names => {}}
+  
 
   
 end
