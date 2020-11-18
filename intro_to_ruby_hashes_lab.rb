0753c3990155d36c3c927376a95444ca192bbb16
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-18 22:31:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ def monopoly_with_second_tier
   # complexity.
   
   
- # {[:railroads]} = {:pieces => 4, :rent_in_dollars => {}, :names => {}}
+ {:railroads} = {:pieces => 4, :rent_in_dollars => {}, :names => {}}
 
   
 end
